e69a309a1656e49c9f5d2e29f0e8dc5953e3203c
wagmitasharma <wagmitasharma1998@gmail.com>
2021-03-20 04:28:15 +0530

    login

--- src/main/java/com/gonuclei/hackathonGroot/HackathonGrootApplication.java
+++ src/main/java/com/gonuclei/hackathonGroot/HackathonGrootApplication.java
@@ -1,8 +1,14 @@
 package com.gonuclei.hackathonGroot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
+@EnableAutoConfiguration()
+@Component
 @SpringBootApplication
 public class HackathonGrootApplication {
 
--- src/main/java/com/gonuclei/hackathonGroot/UserRepoService.java
+++ src/main/java/com/gonuclei/hackathonGroot/UserRepoService.java
@@ -3,6 +3,7 @@ package com.gonuclei.hackathonGroot;
 import com.gonuclei.hackathonGroot.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Repository
 public interface UserRepoService extends JpaRepository<Users, Long> {
